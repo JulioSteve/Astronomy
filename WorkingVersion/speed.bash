5ad1ba9594d07cd02c3cc776c83961b3c0a01d38
JulioSteve <jules.stevenot25@gmail.com>
2025-06-25 21:00:18 +0200

    gitignore v2

--- WorkingVersion/speed.bash
+++
@@ -1,8 +0,0 @@
-for ((j=0; j<=10; j++)); do
-    for ((i=1; i<=16; i++)); do
-        export  OMP_NUM_THREADS=$i
-        time=$( TIMEFORMAT="%R"; { time ./main; } 2>&1 | sed -e 's/\,/./g')
-        echo $time >> time$j.dat
-    done
-    echo "File $j done!"
-done
